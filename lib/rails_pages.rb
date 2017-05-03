49fc494dc50a0c296505b00f4e051bf15967bce9
afeiship <1290657123@qq.com>
2017-05-03 22:42:35 +0800

    update

--- lib/rails_pages.rb
+++ lib/rails_pages.rb
@@ -1,5 +1,12 @@
 require "rails_pages/version"
 
 module RailsPages
-  # Your code goes here...
+  class PagesController < ApplicationController
+      pages = %w[ page1 page2 page3 ]
+      pages.each do |page|
+          define_method(page) do
+              #do nothing
+          end
+      end
+  end
 end
