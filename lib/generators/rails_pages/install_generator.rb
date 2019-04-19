b7f46455ead4c44d8e767b848256aa5fabedd6ed
afeiship <1290657123@qq.com>
2019-04-19 22:29:22 +0800

    fix: app/pages page

--- lib/generators/rails_pages/install_generator.rb
+++ lib/generators/rails_pages/install_generator.rb
@@ -4,8 +4,9 @@ module RailsPages
     desc "Configure necessary files to use RailsPages"
 
     def create_pages
+      copy_file "views/application.html.erb", "app/views/layouts/rails_pages/application.html.erb"
       RailsPages.configuration.pages.each do |page|
-        copy_file "page.html.erb", "app/views/rails_pages/pages/#{page}.html.erb"
+        copy_file "views/page.html.erb", "app/views/rails_pages/pages/#{page}.html.erb"
       end
     end
   end
